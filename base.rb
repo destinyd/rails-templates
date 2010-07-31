74d8c094f2cd2f71461b1346e7421fa45ce32622
destinyd <destinyd_war@163.com>
2010-07-31 14:55:55 +0800

    more powerful base

--- base.rb
+++ base.rb
@@ -1,24 +1,43 @@
 generate :nifty_layout
 
+run "echo 'TODO add readme content' > README"
+run "cp config/database.yml config/database.yml.example"
+
+run "rm public/index.html"
+run "rm public/favicon.ico"
+run "rm public/robots.txt"
+run "rm public/images/rails.png"
+run "rm -f public/javascripts/*"
+# Download JQuery
+run "curl -s -L http://jqueryjs.googlecode.com/files/jquery-1.3.1.min.js > public/javascripts/jquery.js"
+run "curl -s -L http://jqueryjs.googlecode.com/svn/trunk/plugins/form/jquery.form.js > public/javascripts/jquery.form.js"
+
 git :init
 
+run "touch tmp/.gitignore log/.gitignore vendor/.gitignore"
+
 gem "formtastic"
-rake "gems:install"
+rake "gems:install",:sudo => true
 generate :formtastic
+
+plugin 'will_paginate', :git => 'git://github.com/mislav/will_paginate.git'
 
 run 'sed -i "s/stylesheet_link_tag \'application\' /stylesheet_link_tag \'application\',\'formtastic\',\'formtastic_change\' /g" app/views/layouts/application.html.erb'
 
-run "echo 'TODO add readme content' > README"
-run "touch tmp/.gitignore log/.gitignore"
-run "cp config/database.yml config/example_database.yml"
-git :rm => "public/images/rails.png"
-run :rm => "public/index.html"
 
 file ".gitignore", <<-END
 log/*.log
 tmp/**/*
 config/database.yml
 db/*.sqlite3
+db/*.db
+db/schema.rb
+.DS_Store
+doc/api
+doc/app
+public/javascripts/all.js
+public/stylesheets/all.js
+nbproject/**/*
 END
 
 git :add => "."
