496623b2e7e78b3a03ef3ac3391a060403e0be52
destinyd <destinyd_war@163.com>
2010-07-31 13:07:02 +0800

    fix zh-cn bug to :"zh-cn"

--- zh-cn.rb
+++ zh-cn.rb
@@ -1,4 +1,4 @@
 load_template "http://github.com/destinyd/rails-templates/raw/master/base.rb"
 run "curl http://github.com/destinyd/rails-templates/raw/master/locales/zh-cn.yml -o config/locales/zh-cn.yml"
 #run 'sed -i "s/# config.i18n.default_locale = :de/config.i18n.default_locale = :zh-cn/g" "grep '# config.i18n.default_locale = :de' -rl config/"'
-run 'sed -i "s/# config.i18n.default_locale = :de/config.i18n.default_locale = :zh-cn/g" config/environment.rb'
+run 'sed -i "s/# config.i18n.default_locale = :de/config.i18n.default_locale = :\"zh-cn\"/g" config/environment.rb'
