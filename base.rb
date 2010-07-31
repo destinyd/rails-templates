21629b574ba2d5f2d6c6afd28b7e27bfcd309efd
destinyd <destinyd_war@163.com>
2010-07-31 12:48:08 +0800

    add rm index and rails.png

--- base.rb
+++ base.rb
@@ -5,6 +5,8 @@ git :init
 run "echo 'TODO add readme content' > README"
 run "touch tmp/.gitignore log/.gitignore"
 run "cp config/database.yml config/example_database.yml"
+run "rm public/images/rails.png"
+run "rm public/index.html"
 
 file ".gitignore", <<-END
 log/*.log
